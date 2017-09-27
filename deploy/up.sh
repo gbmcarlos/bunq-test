e5a3348697ad322b2385d4cd999cbf11cbe0351d
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2017-09-27 05:45:37 +0400

    pass local or environment db credentials

--- deploy/up.sh
+++ deploy/up.sh
@@ -7,5 +7,9 @@ echo ">>>> Running"
 
 cd www && composer install --prefer-dist --no-interaction --ignore-platform-reqs && cd ..
 docker rm -f chat_app || true
-pwd
-docker run --name chat_app -e APP_ENV=local -e DB_PASSWORD=password -d -p $1:80 -v $PWD/www:/var/www/html chat_app:latest
+g
+if [[ $1 == local ]]; then
+    docker run --name chat_app -e APP_ENV=local -e DB_HOST=127.0.0.1 DB_USER=user DB_PASSWORD=password -d -p $1:80 -v $PWD/www:/var/www/html chat_app:latest
+else
+    docker run --name chat_app -e APP_ENV=$1 -e DB_HOST=$DB_HOST DB_USER=$DB_USER DB_PASSWORD=password -d -p $1:80 -v $PWD/www:/var/www/html chat_app:latest
+fi
