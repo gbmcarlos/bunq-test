819eddcdf70d47d72888928d16996e525a3767bb
Carlos Garcia-Berro Molero <gbmcarlos@gmail.com>
2017-09-29 04:23:33 +0400

    app.php reorganised and commented, now setting the debug and registering the db connection (the pdo object) in the app's service container

--- www/web/app.php
+++ www/web/app.php
@@ -8,12 +8,22 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+// Create app
+$app = new Silex\Application();
+
+// Set debug from env var
+$app['debug'] = getenv('APP_DEBUG');
+
+// Set up db connections and set schema
 $db = new DB\SQLiteDatabase();
 $db->connect(getenv('SQLITEDB_FILE'));
 $schema = require_once(__DIR__.'/../db/schema.php');
 $db->create($schema);
 
-$app = new Silex\Application();
+// Register db in the service container
+$app['DB'] =  function() use ($db) {
+    return $db->getPDO();
+};
 
 $app->get('/hello/{name}', function ($name) use ($app, $db) {
     return var_dump($db->getTableList());
